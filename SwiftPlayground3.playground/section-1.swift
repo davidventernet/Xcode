cab1ade26f8712c7757b42c98380ee15ed07a7ca
David Venter <david@davidventer.net>
2015-03-15 01:12:15 +0200

    Swift

---
+++ SwiftPlayground3.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Bool (Bolean: True/False, 0/1)
+
+// True or False?
+
+var truth : Bool
+truth = true
+
+// Math: Is it Greater or Less Than?
+
+let myMath = 6 > 3
+print(myMath)
+
+// Or is it equal?
+
+let one = 1
+let myComparison = 1 > 2
+print(myComparison)
+
+// It is equal...
+
+let five = 5
+let myEqual = five == 5
+print(myEqual)
+
+// ! = The opposite of the Truth
+
+let hisTruth = true
+let myTruth = !hisTruth
+print(myTruth)
+
+// AND
